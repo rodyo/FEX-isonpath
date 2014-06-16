2d4db5b01698e53cd82e57e0072ee9039bbf1838
Rody Oldenhuis <oldenhuis@gmail.com>
2014-06-16 10:02:11 +0200

    Updated licence info.

--- isonpath.m
+++ isonpath.m
@@ -20,7 +20,7 @@ function yn = isonpath(dirs)
 % E-mail     : oldenhuis@gmail.com    (personal)
 %              oldenhuis@luxspace.lu  (professional)
 % Affiliation: LuxSpace sàrl
-% Licence    : GPL + anything implied by placing it on the FEX
+% Licence    : BSD
 
 
 % Changelog
